17bd20df93f24fa006b2afe56a281ce688892a65
Mario Bruno <mbrunostem@gmail.com>
2021-12-24 16:05:52 -0500

    setting up initial schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  roles_name VARCHAR(30) NOT NULL
+  salary DECIMAL NOT NULL
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL
+  last_name VARCHAR(30) NOT NULL
+  FOREIGN KEY (roles_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL
+);
